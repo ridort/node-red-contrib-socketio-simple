e834bd06bb7d4d22b6897c17429aa8259ac46c7a
Ryan <ryan.sheppard@outlook.com>
2022-11-09 12:47:40 +1030

    Initial support for room join/leave

---
+++ src/socketio-leave.js
@@ -0,0 +1,40 @@
+const parseTypedInputs = require("./util/parseTypedInputs");
+
+module.exports = function (RED) {
+  function NodeFunction(config) {
+    // *************************************************************************
+    // Setup
+    // *************************************************************************
+    RED.nodes.createNode(this, config);
+    const node = this;
+
+    // Node inputs
+    node.inServer = RED.nodes.getNode(config.server);
+    node.inRoom = config.room;
+    node.inRoomType = config.roomType;
+    node.inTarget = config.target;
+    node.inTargetType = config.targetType;
+
+    // *************************************************************************
+    // Node logic
+    // *************************************************************************
+    // Called when a message is passed into the node
+    const inputHandler = (msg, send, done) => {
+      const io = node.inServer.io;
+
+      const room = parseTypedInputs(node.inRoom, node.inRoomType, msg, RED);
+      const target = parseTypedInputs(node.inTarget, node.inTargetType, msg, RED);
+
+      io.sockets.get(target).leave(room);
+
+      node.send(msg);
+    };
+
+    // *************************************************************************
+    // Listener management
+    // *************************************************************************
+    node.on("input", inputHandler);
+  }
+
+  RED.nodes.registerType("socket.io leave", NodeFunction);
+};
